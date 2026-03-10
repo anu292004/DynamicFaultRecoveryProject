fa932843c231bcb238656dc7e549936f0e1d9955
anu292004 <kharseanuli@gmail.com>
2026-03-11 00:36:49 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -11,7 +11,7 @@ from plotly.subplots import make_subplots
 
 st.set_page_config(
     page_title="AGV Fleet Manager",
-    page_icon="🤖➡️📦",
+    page_icon="🤖",
     layout="wide",
     initial_sidebar_state="expanded"
 )
